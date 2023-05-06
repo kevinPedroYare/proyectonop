8e88af065cfeb125eea8365f75ef7d342b151e4a
unknown <kyare@unsa.edu.pe>
2023-05-06 13:29:22 -0500

    saludo mejorado

--- saludo.java
+++ saludo.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 public class saludo {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("Escriba su nombre");
         System.out.println("Hola " + sc.nextLine() + "!");
     }
 }
