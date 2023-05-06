adf05f160b3836f7a0786f39c848042610922443
unknown <kyare@unsa.edu.pe>
2023-05-06 14:34:13 -0500

    Math

---
+++ Math.java
@@ -0,0 +1,16 @@
+public class Math{
+    public static void main(String[] args){
+        //Math
+        //Math.max(x,y) retorna el numero mas grande
+        Math.max(5, 10);
+        //Math.min(x,y) retorna el numero mas pequeño
+        Math.min(5, 10);
+        //Math.sqrt(x) retorna la raiz cuadrada de x
+        Math.sqrt(64);
+        //Math.abs(x) retorna el valor absoluto de x
+        Math.abs(-4.7);
+        //Math.random() retorna un numero aleatorio entre 0 y 1
+        Math.random();
+        int randomNum = (int)(Math.random() * 101);  // 0 to 100
+    }
+}
