10f33a507a41acefa53a416c3b811243a64bee68
unknown <kyare@unsa.edu.pe>
2023-05-06 13:27:38 -0500

    saludo

---
+++ saludo.java
@@ -0,0 +1,7 @@
+import java.util.Scanner;
+public class saludo {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Hola " + sc.nextLine() + "!");
+    }
+}
